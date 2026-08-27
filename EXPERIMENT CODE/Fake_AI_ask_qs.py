9bd5d6e529e6db7dd83efbb315aa21a99484a74f
MITALI <mitaalee.mili@gmail.com>
2026-08-27 13:15:03 +0530

    Delete EXPERIMENT CODE directory

--- EXPERIMENT CODE/Fake_AI_ask_qs.py
+++
@@ -1,7 +0,0 @@
-question=[
-    "what is your name",
-    "introduce yourself",
-    "explain udp"
-]
-for q in question:
-    print(q)
--- EXPERIMENT CODE/convert_speech_to_text.py
+++
@@ -1,5 +0,0 @@
-from faster_whisper import WhisperModel
-model=WhisperModel("base")  #balanced size of whisper
-segments, info=model.transcribe(r"c:\Users\mitaa\Downloads\sundari.mp3")
-for s in segments:
-    print(s.text)
